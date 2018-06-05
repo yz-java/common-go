795305f13488ae289ff24db0a802ed1bc8e5f724
杨钊 <yangzhao_java@163.com>
2018-06-05 15:01:37 +0800

    添加log工具类、反射工具类、map转结构体对象

---
+++ log/logger.go
@@ -0,0 +1,31 @@
+package log
+
+import (
+	"os"
+	"github.com/op/go-logging"
+	"time"
+)
+var Logger = logging.MustGetLogger("logger")
+
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05} %{shortfile} %{longfunc} >>> %{level:.4s} %{id:04d} %{message}%{color:reset}`,
+)
+func CreateLog(path string) {
+	logFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+	if err != nil {
+		Logger.Fatal("open Logger file error", err)
+	}
+	backend1 := logging.NewLogBackend(logFile, "", 0)
+	backend2 := logging.NewLogBackend(os.Stderr, "", 0)
+
+	backend1Formatter := logging.NewBackendFormatter(backend1, format)
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+
+	logging.SetBackend(backend1Formatter, backend2Formatter)
+	Logger.Notice("服务启动:", time.Now().Format("2006-01-02 15:04:05"))
+
+}
+
+
+
+
---
+++ utils/map_util.go
@@ -0,0 +1,47 @@
+package utils
+
+
+import (
+	"reflect"
+	"fmt"
+	"./reflect_util"
+)
+
+//用map填充结构
+func MapToStruct(data map[string]interface{}, obj interface{}) error {
+	for k, v := range data {
+		err := SetField(obj, k, v)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//用map的值替换结构的值
+func SetField(obj interface{}, name string, value interface{}) error {
+	structValue := reflect.ValueOf(obj).Elem()        //结构体属性值
+	structFieldValue := structValue.FieldByName(name) //结构体单个属性值
+
+	if !structFieldValue.IsValid() {
+		return fmt.Errorf("No such field: %s in obj", name)
+	}
+
+	if !structFieldValue.CanSet() {
+		return fmt.Errorf("Cannot set %s field value", name)
+	}
+
+	structFieldType := structFieldValue.Type() //结构体的类型
+	val := reflect.ValueOf(value)              //map值的反射值
+
+	var err error
+	if structFieldType != val.Type() {
+		val, err = reflect_util.TypeConversion(fmt.Sprintf("%v", value), structFieldValue.Type().Name()) //类型转换
+		if err != nil {
+			return err
+		}
+	}
+
+	structFieldValue.Set(val)
+	return nil
+}
